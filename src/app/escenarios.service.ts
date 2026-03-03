c8e1639b9b6cda7234659b77c831762855bbe533
Javier Sobrino <Javier.Sobrino1@alu.uclm.es>
2026-03-03 18:01:13 +0100

    feat: enhance Escenarios service and component with error handling and observable types

--- src/app/escenarios.service.ts
+++ src/app/escenarios.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,15 @@ import { Injectable } from '@angular/core';
 export class EscenariosService {
   constructor(private http: HttpClient) {}
 
-  getEscenarios() {
-    return this.http.get('http://localhost:8080/busqueda/getEscenarios');
+  getEscenarios(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost:8080/busqueda/getEscenarios');
+  }
+
+  getEspectaculos(escenario: any) {
+    return this.http.get(`http://localhost:8080/busqueda/getEspectaculos/${escenario.id}`);
+  }
+
+  getEntradas(espectaculo: any) {
+    return this.http.get(`http://localhost:8080/busqueda/getEntradas?espectaculoId=${espectaculo.id}`);
   }
 }
--- src/app/escenarios/escenarios.ts
+++ src/app/escenarios/escenarios.ts
@@ -1,23 +1,55 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { EscenariosService } from '../escenarios.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-escenarios',
-  imports: [],
+  imports: [CommonModule],
   providers: [EscenariosService],
   templateUrl: './escenarios.html',
   styleUrl: './escenarios.css',
 })
 export class Escenarios {
-  constructor(private escenariosService: EscenariosService) {}
+  
+  escenarios: any = [];  // Any se usa para unificar el tipo de datos, ya que no se ha definido un modelo específico para escenarios, espectaculos o entradas
 
-  getEscenarios() {
+  constructor(
+    private escenariosService: EscenariosService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  getEscenarios() {  // En una llamada http podemos obtener response (OK) o un error (KO), por eso se usan dos parámetros en el subscribe, uno para cada caso
     this.escenariosService.getEscenarios().subscribe(
       (response) => {
-        console.log('BackEnd Response -> Escenarios:', response);
+        this.escenarios = response;
+        this.cdr.detectChanges();  // Evita dos llamadas a detectChanges() en el mismo ciclo de detección evitando errores
       },
       (error) => {
         console.error('Error fetching escenarios:', error);
+      }
+    );
+  }
+
+  getEspectaculos(escenario: any) {
+    this.escenariosService.getEspectaculos(escenario).subscribe(
+      (response: any) => {
+        escenario.espectaculos = response;
+        this.cdr.detectChanges();
+      },
+      (error: any) => {
+        console.error('Error fetching espectaculos:', error);
+      }
+    );
+  }
+
+  getEntradas(espectaculo: any) {
+    this.escenariosService.getEntradas(espectaculo).subscribe(
+      (response: any) => {
+        espectaculo.entradas = response;
+        this.cdr.detectChanges();
+      },
+      (error: any) => {
+        console.error('Error fetching entradas:', error);
       }
     );
   }
